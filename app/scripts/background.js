9ba798e06e8e9d345b250537fd056e3ce865d39c
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-01 19:23:25 -0500

    use 2 spaces for indentation

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,18 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
+chrome.runtime.onInstalled.addListener(function(details) {
+  console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.pageAction.show(tabId);
+chrome.tabs.onUpdated.addListener(function(tabId) {
+  chrome.pageAction.show(tabId);
+});
+
+chrome.contextMenus.create({
+  title: 'Find protractor selector',
+  onclick: function(info, tab) {
+    console.log('clicked ---', info, tab);
+  }
+}, function() {
+
 });
